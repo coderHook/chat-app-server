fc0e73b2f73b9a874e43d231111d39b4ece69bcf
coderhook <abel1987@hotmail.es>
2019-07-05 14:01:29 +0200

    Add server to heroku

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+//allow cors
+app.use(cors())
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+// this acts as our database
+const messages = [
+  'Hello',
+  'Can you see this?'
+]
+
+//serialize the data
+const json = JSON.stringify(messages) // turn the array into a single string
+const stream = new Sse(json) //When someone connects send this message in a string.
+
+function onStream(request, response){
+  stream.init(request, response) //this connects the client to the server
+}
+
+app.get('/stream', onStream)
+
+function onMessage(request, response) {
+  const { message } = request.body
+
+  messages.push(message);
+
+  const json = JSON.stringify(messages)
+
+  //update the initial data
+  stream.updateInit(json);
+
+  //Notify all the clients.
+  stream.send(json)
+
+  //send Response
+  return response.status(201).send(message)
+}
+
+app.post('/message', onMessage)
+
+const port = 5000
+
+function onListen(){
+  console.log(`Listening on port ${port}`)
+}
+
+app.listen(port, onListen)
